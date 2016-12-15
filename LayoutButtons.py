c6c6bddf3ea0166d6faa1fe7aa16399a354b0efb
Christian <christianherz1985@googlemail.com>
2016-12-15 13:10:26 -0500

    ENH: Enabling option to specify layers that are displayed for the buttons
    - example:
      module = slicer.modules.LayoutButtonsWidget
      module.setVisibleLayers(["Foreground"]) # for displaying only Foreground layer

--- LayoutButtons.py
+++ LayoutButtons.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import vtk, qt, ctk, slicer
+from collections import OrderedDict
 from slicer.ScriptedLoadableModule import *
 from SlicerProstateUtils.mixins import *
 
@@ -36,14 +37,70 @@ class LayoutButtonsWidget(ModuleWidgetMixin, ScriptedLoadableModuleWidget):
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
 
+  _AVAILABLE_LAYERS = OrderedDict([("Label", "vtkMRMLLabelMapVolumeNode"),
+                                   ("Foreground", "vtkMRMLScalarVolumeNode"),
+                                   ("Background", "vtkMRMLScalarVolumeNode")])
+
+  @property
+  def layerNameVolumeClassPairs(self):
+    return self._layerNameVolumeClassPairs
+
+  @layerNameVolumeClassPairs.setter
+  def layerNameVolumeClassPairs(self, dictionary):
+    """
+      example:
+        from collections import OrderedDict
+        {object}.layerNameVolumeClassPairs = OrderedDict([("Label", "vtkMRMLLabelMapVolumeNode")])
+    """
+    if not isinstance(dictionary, OrderedDict):
+      raise ValueError("Parameter needs to be a %s" % str(OrderedDict))
+    for layerName, volumeClass in dictionary.iteritems():
+      getterName = self.getCompositeGetterNameForLayer(layerName)
+      if not hasattr(slicer.vtkMRMLSliceCompositeNode, getterName):
+        raise ValueError("{0} is not valid. Method {1} does not exist in {2}".format(layerName, getterName,
+                                                                                     slicer.vtkMRMLSliceCompositeNode))
+      elif not hasattr(slicer, volumeClass):
+        raise ValueError("VolumeClass {} is not valid or doesn't exist.".format(volumeClass))
+    self._layerNameVolumeClassPairs = dictionary
+    self.refresh()
+
+  def getCompositeGetterNameForLayer(self, layerName):
+    return "Get{}VolumeID".format(layerName)
+
+  def getCompositeSetterNameForLayer(self):
+    return "Set{}VolumeID".format(layerName)
+
   def __init__(self, parent=None):
     ScriptedLoadableModuleWidget.__init__(self, parent)
-    self.buttons = []
     self.layoutLogic = self.layoutManager.layoutLogic()
     self.lNode = self.layoutLogic.GetLayoutNode()
+    self.fitSliceToAll = False
+    self._layerNameVolumeClassPairs = self._AVAILABLE_LAYERS
+    self._buttons = []
+    self._setupPropertyNames()
+
+  def _setupPropertyNames(self):
+    self._propertyNames = ["fitSliceToAll", "layerNameVolumeClassPairs"]
+
+  def setVisibleLayers(self, layerNames):
+    allowedLayerNames = self._AVAILABLE_LAYERS.keys()
+
+    def checkLayerNames(names):
+      for name in names:
+        if not name.istitle(): raise ValueError("'%s': All layer names need to start with a capital letter" % name)
+        if not name in allowedLayerNames: raise KeyError("Unknown layer name {0}. {0} does not exist. Available layers:"
+                                                         " {1}".format(name, str(allowedLayerNames)))
+    checkLayerNames(layerNames)
+    layerSubset = self._AVAILABLE_LAYERS.fromkeys(layerNames)
+    for layerName in layerNames:
+      layerSubset[layerName] = self._AVAILABLE_LAYERS[layerName]
+    self.layerNameVolumeClassPairs = layerSubset
+
+  def getProperties(self):
+    return [{"attributeName": name, "value": getattr(self, name)} for name in self._propertyNames]
 
   def cleanup(self):
-    self.layoutManager.layoutChanged.disconnect(self.onLayoutChanged)
+    self.layoutManager.layoutChanged.disconnect(self.refresh)
     self.removeLayoutButtons()
 
   def setup(self):
@@ -81,10 +138,13 @@ class LayoutButtonsWidget(ModuleWidgetMixin, ScriptedLoadableModuleWidget):
     button = self.createButton(name, name=name, enabled=isSliceNode)
     button.setSizePolicy(qt.QSizePolicy.Minimum, qt.QSizePolicy.Minimum)
     button.setStyleSheet("QPushButton,QMenu{background-color:%s;}" % self.getColorFromProperties(child))
-    self.buttons.append(button)
+    self._buttons.append(button)
     if isSliceNode:
       self.addMenu(button)
     return button
+
+  # def generateName(self, button):
+
 
   def getColorFromProperties(self, element):
     for elemProp in element.getchildren():
@@ -100,23 +160,23 @@ class LayoutButtonsWidget(ModuleWidgetMixin, ScriptedLoadableModuleWidget):
 
   def onMenuSelected(self, menu):
     menu.clear()
-    self.addSubMenu(menu, "Foreground")
-    self.addSubMenu(menu, "Background")
+    for layerName, volumeClass in self._layerNameVolumeClassPairs.iteritems():
+      self.addSubMenu(menu, layerName, volumeClass)
 
-  def addSubMenu(self, menu, layer):
+  def addSubMenu(self, menu, layer, volumeClass):
     subMenuBackground = qt.QMenu(layer, menu)
     menu.addMenu(subMenuBackground)
     actionGroup = qt.QActionGroup(menu)
     actionGroup.setExclusive(True)
 
     _, cNode = self.getCompositeNodeAndWidgetByName(menu.name)
-    for volume in [None]+self.getAvailableScalarVolumes():
+    for volume in [None]+self.getAvailableVolumes(volumeClassName=volumeClass):
       action = qt.QAction(volume.GetName() if volume else "None", actionGroup)
       subMenuBackground.addAction(action)
       actionGroup.addAction(action)
       action.setCheckable(True)
       action.triggered.connect(lambda triggered, l=layer, n=menu.name,v=volume: self.onImageSelectedFromMenu(l,n,v))
-      currentVolumeID = getattr(cNode, "Get{}VolumeID".format(layer))()
+      currentVolumeID = getattr(cNode, self.getCompositeGetterNameForLayer(layer))()
       imageID = volume.GetID() if volume else volume
       if currentVolumeID == imageID:
         action.setChecked(True)
@@ -124,28 +184,29 @@ class LayoutButtonsWidget(ModuleWidgetMixin, ScriptedLoadableModuleWidget):
   def onImageSelectedFromMenu(self, layer, viewName, volume):
     widget, cNode = self.getCompositeNodeAndWidgetByName(viewName)
     getattr(cNode, "Set{}VolumeID".format(layer))(volume.GetID() if volume else None)
-    widget.sliceLogic().FitSliceToAll()
+    if self.fitSliceToAll:
+      widget.sliceLogic().FitSliceToAll()
 
   def getCompositeNodeAndWidgetByName(self, name):
     widget = self.layoutManager.sliceWidget(name)
     return widget, widget.mrmlSliceCompositeNode()
 
-  def getAvailableScalarVolumes(self):
+  def getAvailableVolumes(self, volumeClassName):
     # TODO: override this for setting specific images only
-    return slicer.util.getNodesByClass("vtkMRMLScalarVolumeNode")
+    return slicer.util.getNodesByClass(volumeClassName)
 
   def removeLayoutButtons(self):
     self.buttonWidget.layout().removeWidget(self.buttonLayoutGroup)
     self.buttonLayoutGroup.deleteLater()
-    self.buttons = []
+    self._buttons = []
     self.menus = []
 
   def getVisibleWidgets(self):
     pass
 
   def setupConnections(self):
-    self.layoutManager.layoutChanged.connect(self.onLayoutChanged)
+    self.layoutManager.layoutChanged.connect(self.refresh)
 
-  def onLayoutChanged(self):
+  def refresh(self):
     self.removeLayoutButtons()
     self.addLayoutButtons()
